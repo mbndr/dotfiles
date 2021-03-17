5a62b7da991f01fb4b22f99a995a59e1ce569e57
mbndr <probandula@posteo.de>
2021-03-17 08:40:42 +0100

    update key bindings

--- README.md
+++ README.md
@@ -5,3 +5,48 @@
 Dotfiles can be setup with gnu `stow /*`.
 
 Config files for st and dwm are provided but maybe some patches have to be applied...
+
+```
+# Spawning stuff
+Super + p							dmenu
+Super + {Return,t}					terminal
+Super + w							firefox
+Super + n							notes.txt
+super + a							audio mixer
+
+# Window manager
+Alt + B								toggle bar
+Alt + {j,k}							move focus
+Alt + {h,l}							make master {smaller,larger}
+Alt + Return						window to master
+Alt + q								kill window
+Alt + {t,m,f}						set {tiled,monocle,floating} layout
+Alt + Space							toggle window floating
+Alt + {1,2,3,4,5}					show {n}-th tag
+Alt + 0								show all tags
+Alt + Shift + {1,2,3,4,5}			move window to {n}-th tag
+Alt + Shift + 0						move window to all tags
+Alt + {Comma,Period}				move monitor focus
+Alt + Shift + q						restart dwm
+Alt + {Plus,Minus}					set gap size
+
+LeftMouse(layoutsymbol)				toggle tiled,monocle layout
+LeftMouse(tagbar)					show exclusively this tag
+RightMouse(tagbar)					add tag to shown tags
+
+LeftMouse(window) + Alt				move window (and set to floating)
+RightMouse(window) + Alt			change window size (and set to floating)
+MiddleMouse(window) + Alt			toggle window floating
+LeftMouse(window) + Alt + Shift		set window to master
+
+LeftMouse(tagbar) + Alt				move focused window exclusively to tag
+RightMouse(tagbar) + Alt			add focused window to tag
+
+# Media control (default media keys also work)
+MediaPlay							play / pause spotify
+MediaPlay + Ctrl					play / pause firefox
+Super + Right						next spotify song
+Super + Left						prev spotify song
+Super + Ctrl + {j,k}				volume {down,up}
+Super + Ctrl + m					toggle microphone mute state
+```
--- src/.local/share/src/dwm/config.h
+++ src/.local/share/src/dwm/config.h
@@ -46,7 +46,8 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod4Mask
+#define MODKEY Mod1Mask
+#define SuperMask Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -63,56 +64,51 @@ static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } }, // dmenu
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } }, // terminal
-	{ MODKEY,                       XK_b,      togglebar,      {0} }, // toggle topbar
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } }, // move focus
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } }, // move focus
-	//{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	//{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} }, // move master size
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} }, // move master size
-	{ MODKEY,                       XK_Return, zoom,           {0} }, // move selected to master
-	//{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY,                       XK_q,      killclient,     {0} }, // kill window
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} }, // set tiling layout
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} }, // set floating layout
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} }, // set monocle layout
-	//{ MODKEY,                       XK_space,  setlayout,      {0} },
-	{ MODKEY,                       XK_space,  togglefloating, {0} }, // toggle selected float
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },// display all tags
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } }, // move window to tag
-	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } }, // change monitor focus
-	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } }, // change monitor focus
-	//{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	//{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	TAGKEYS(                        XK_1,                      0) // display tag X...
+	
+	// Spawning (rest in sxhkd)
+	{ SuperMask,                    XK_p,      spawn,          {.v = dmenucmd } },
+	{ SuperMask,                    XK_Return, spawn,          {.v = termcmd } },
+
+	// Dwm control
+	{ MODKEY,                       XK_b,      togglebar,      {0} },
+	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
+	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
+	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
+	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
+	{ MODKEY,                       XK_Return, zoom,           {0} },
+	{ MODKEY,                       XK_q,      killclient,     {0} },
+	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
+	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
+	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_space,  togglefloating, {0} },
+	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
+	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
+	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
+	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ MODKEY,			            XK_minus,  setgaps,        {.i = -5} },
+	{ MODKEY,			            XK_plus,   setgaps,        {.i = +5} },
+	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
 	TAGKEYS(                        XK_4,                      3)
 	TAGKEYS(                        XK_5,                      4)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} }, // quit dwm (restarts)
-	// Gaps
-	{ MODKEY,			XK_minus,  setgaps,        {.i = -5} },
-	{ MODKEY,			XK_plus,   setgaps,        {.i = +5} },
-	{ MODKEY,			XK_equal,  setgaps,        {.i = GAP_RESET} },
 };
 
 /* button definitions */
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
 static Button buttons[] = {
-	/* click                event mask      button          function        argument */
+	/* click                event mask        button          function        argument */
 	{ ClkLtSymbol,          0,                Button1,        setlayout,      {0} },
-	{ ClkLtSymbol,          0,                Button3,        setlayout,      {.v = &layouts[2]} },
-	{ ClkWinTitle,          0,                Button2,        zoom,           {0} },
-	{ ClkStatusText,        0,                Button2,        spawn,          {.v = termcmd } },
+	{ ClkTagBar,            0,                Button1,        view,           {0} },
+	{ ClkTagBar,            0,                Button3,        toggleview,     {0} },
+	
 	{ ClkClientWin,         MODKEY,           Button1,        movemouse,      {0} },
 	{ ClkClientWin,         MODKEY,           Button2,        togglefloating, {0} },
 	{ ClkClientWin,         MODKEY,           Button3,        resizemouse,    {0} },
-	{ ClkTagBar,            0,                Button1,        view,           {0} },
-	{ ClkTagBar,            0,                Button3,        toggleview,     {0} },
+	{ ClkClientWin,         MODKEY|ShiftMask, Button1,        zoom,           {0} },
+
 	{ ClkTagBar,            MODKEY,           Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,           Button3,        toggletag,      {0} },
-	{ ClkClientWin,         MODKEY|ShiftMask, Button1,        zoom,           {0} },
 };
 
